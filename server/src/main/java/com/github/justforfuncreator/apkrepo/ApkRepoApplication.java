ad6e19bc20579acf6b7b6592703e560bf15a95ca
just-for-fun-creator <190646835+just-for-fun-creator@users.noreply.github.com>
2025-07-16 22:43:26 +0300

    Initialize server

---
+++ server/src/main/java/com/github/justforfuncreator/apkrepo/ApkRepoApplication.java
@@ -0,0 +1,13 @@
+package com.github.justforfuncreator.apkrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApkRepoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/github/justforfuncreator/apkrepo/ApkRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.justforfuncreator.apkrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApkRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
